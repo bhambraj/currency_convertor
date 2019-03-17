b1c052cf568d32834b7e6d83179d8344cc6499c1
Janu-Bhambra <janujsb@ymail.com>
2019-03-16 20:03:00 -0400

    Fixed server to use Heroku's dynamic PORT

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const port = process.env.port | 5000;
+const port = process.env.PORT || 5000;
 /*
  *   Calculates exchange rates for input from the request
  *   Conversion Algorithm: input X (2 + random markup between 0 and 1)
